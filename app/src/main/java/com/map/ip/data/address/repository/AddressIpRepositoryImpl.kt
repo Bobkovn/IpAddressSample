ec6631f8e589761af1c307c1b8e56847ab18525e
Nikita Bobkov <66723675+bobkovnikita@users.noreply.github.com>
2021-03-04 10:51:52 +0200

    refactor repository

--- app/src/main/java/com/map/ip/data/address/repository/AddressIpRepositoryImpl.kt
+++ app/src/main/java/com/map/ip/data/address/repository/AddressIpRepositoryImpl.kt
@@ -11,29 +11,6 @@ class AddressIpRepositoryImpl @Inject constructor(
     private val addressIpDataSource: AddressIpDataSource
 ) : AddressIpRepository {
 
-    companion object {
-
-        @JvmStatic
-        @Volatile
-        private var INSTANCE: AddressIpRepository? = null
-
-        @JvmStatic
-        fun init(
-            addressIpDataSource: AddressIpDataSource,
-        ) {
-            if (INSTANCE == null) {
-                INSTANCE = AddressIpRepositoryImpl(addressIpDataSource)
-            }
-        }
-
-        @JvmStatic
-        fun getInstance(): AddressIpRepository = synchronized(this) {
-            INSTANCE?.let {
-                return INSTANCE as AddressIpRepository
-            } ?: throw RuntimeException("need to init Repository")
-        }
-    }
-
     override suspend fun getAddressByIp(ip: String) =
         addressIpDataSource.getAddressByIp(ip)
 }
